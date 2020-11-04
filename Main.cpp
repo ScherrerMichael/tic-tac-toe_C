fc2191e457758f0f6a483be6f96c585494b38f53
Michael Scherrer <michaelscherrer23@gmail.com>
2020-11-03 23:52:38 -0800

    added player data, and welcome function

--- Main.cpp
+++ Main.cpp
@@ -7,15 +7,30 @@ using namespace std;
 const int rows = 3;
 const int columns = 3;
 
+struct player_data
+{
+    bool multiplayer;
+
+    string player_one_name;
+    int player_one_score;
+    char player_one_symbol;
+
+    string player_two_name;
+    int player_two_score;
+    char player_two_symbol;
+};
+
+
+void print_welcome();
+void prompt_create_game(player_data *data);
 void print_board(const char array[][columns]);
 void make_turn(bool player, char symbol, char array[][columns]);
+bool check_win(const char array[][columns]);
 
 int main()
 {
     string input;
-    bool multiplayer;
-    char player_one_symbol;
-    char player_two_symbol;
+    player_data *data = new player_data;
 
     char array[rows][columns] ={
         {' ',' ',' '},
@@ -23,18 +38,15 @@ int main()
         {' ',' ',' '},
     };
 
-    cout<<"welcome, \n one or two players?";
-
-    cout<<"Enter name: ";
-    getline(cin, input);
-    cout<<"Welcome, " + input + "."<<endl;
+    print_welcome();
+    prompt_create_game(data);
 
     do{
         cout<<"do you choose X's or O's?";
         getline(cin,input);
 
         if(input != "X" && input != "O"){
-            cout<<"Please choose a valid input ([X]/[O])."<<endl;
+            cout<<"Please choose a valid input ([X] or [O]).\n"<<endl;
         }
     }while(input != "X" && input != "O");
 
@@ -42,6 +54,56 @@ int main()
 
     make_turn(true,'X', array);
     print_board(array);
+}
+
+void print_welcome(){
+ cout << R"(_____  _  ____     _____  ____  ____     _____  ____  _____
+/__ __\/ \/   _\   /__ __\/  _ \/   _\   /__ __\/  _ \/  __/
+  / \  | ||  / _____ / \  | / \||  / _____ / \  | / \||  \  
+  | |  | ||  \_\____\| |  | |-|||  \_\____\| |  | \_/||  /_ 
+  \_/  \_/\____/     \_/  \_/ \|\____/     \_/  \____/\____\)" <<"\n";
+}
+
+void prompt_create_game(player_data *data){
+
+    int num_players;
+    string input;
+
+    cout<<"Welcome! \n";
+
+    do{
+    cout<<"How many players (1/2)?: ";
+    cin >> num_players;
+    cin.ignore(100,'\n');
+    cout<<"\n";
+
+    if((num_players != 1) && (num_players != 2)){
+        cout <<"\nPlease enter a valid number (1 or 2)";
+    }
+
+    }while((num_players != 1) && (num_players != 2));
+
+    if(num_players == 2)
+    {
+        cout<<"player 1 name: ";
+        getline(cin, data -> player_one_name);
+        cout<<"\n";
+
+        cout<<"player 2 name: ";
+        getline(cin, data -> player_two_name);
+        cout<<"\n";
+    }
+    else{
+        cout<<"Player name: ";
+        getline(cin, data -> player_one_name);
+
+        data -> player_two_name = "CPU";
+    }
+
+    cout<<"\nEnter name: ";
+    getline(cin, input);
+    cout<<"Welcome, " + input + "."<<endl;
+
 }
 
 void print_board(const char array[][columns])
